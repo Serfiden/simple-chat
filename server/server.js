3dad29bd52f2e5053ddc1c160b3e377ef8b2e49f
cristian.silav <cristian.silav@zitec.com>
2018-09-20 09:15:11 +0300

    create new 'server' folder and move all related files inside

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const CHANNELS = {
 }
 
 app.get('/', function(req, res) {
-	res.sendFile(path.resolve(__dirname + '/public/index.html'));
+	res.sendFile(path.resolve('./public/index.html'));
 });
 
 io.on('connection', function(socket) {
